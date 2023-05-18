e14d438a558cf5475ee8f92d52d8b78dbc740799
Leonardorossato <leonardorossato21@gmail.com>
2023-05-18 17:10:51 -0300

    update main.ts swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('Desafio Backend Nestjs')
+    .setDescription('Endpoints da api do Desafio Backend')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.APP_PORT);
 }
-bootstrap();
+void bootstrap();
