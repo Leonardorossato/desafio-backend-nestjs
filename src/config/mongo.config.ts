cd539760745828744a5795dce6ba036f89e12a51
Leonardorossato <leonardorossato21@gmail.com>
2023-05-18 17:07:03 -0300

    update mongo.config.ts ok

--- src/config/mongo.config.ts
+++ src/config/mongo.config.ts
@@ -1,9 +1,13 @@
-import { MongooseModuleAsyncOptions, MongooseModuleOptions } from "@nestjs/mongoose";
+import { ConfigService } from '@nestjs/config';
+import {
+  MongooseModuleAsyncOptions,
+  MongooseModuleOptions,
+} from '@nestjs/mongoose';
 
-export const mongoConnection: MongooseModuleAsyncOptions ={
-    useFactory: async(): Promise<MongooseModuleOptions> =>{
-        return{
-           uri: `{}` 
-        }
-    },
-}
+export const mongoConnection: MongooseModuleAsyncOptions = {
+  useFactory: async (config: ConfigService): Promise<MongooseModuleOptions> => {
+    return {
+      uri: process.env.MONGO_URL,
+    };
+  },
+};
