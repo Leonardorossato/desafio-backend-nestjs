862d0e732f0abc60d453bdee240e7ba5fc88e53c
Leonardorossato <leonardorossato21@gmail.com>
2023-05-18 17:10:54 -0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ImagemModule } from './imagem/imagem.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { mongoConnection } from './config/mongo.config';
+import { ConfigModule } from '@nestjs/config';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
- imports: [ImagemModule]
+  imports: [
+    MongooseModule.forRootAsync(mongoConnection),
+    MulterModule.register({
+      dest: '/uploads',
+    }),
+    ImagemModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
 })
 export class AppModule {}
