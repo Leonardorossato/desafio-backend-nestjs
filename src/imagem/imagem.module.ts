582c4c5cc1ee96e3f2b27e9f3f77f8144203a20f
Leonardorossato <leonardorossato21@gmail.com>
2023-05-18 21:48:15 -0300

    update imagem.module.ts multer

--- src/imagem/imagem.module.ts
+++ src/imagem/imagem.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ImagemService } from './imagem.service';
 import { ImagemController } from './imagem.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Imagem, imagemSchema } from './entities/imagem.entity';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Imagem.name, schema: imagemSchema }]),
+    MulterModule.register({
+      dest: 'uploads/',
+    }),
+  ],
   controllers: [ImagemController],
-  providers: [ImagemService]
+  providers: [ImagemService],
 })
 export class ImagemModule {}
